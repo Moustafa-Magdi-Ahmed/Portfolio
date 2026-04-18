c396219c759a4cf892f96c598413e3b6bb1166df
Mostafa-Magdy-7 <Eng.mostafaalattar@gmail.com>
2026-04-19 01:58:41 +0200

    adding logo

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,22 +11,28 @@ import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Front, E,Nav,Projects,Skills,Stats,Footer],
+  imports: [RouterOutlet, Front, E, Nav, Projects, Skills, Stats, Footer],
   template: `
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz@0,14..32;1,14..32&family=Poppins:wght@600;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <app-nav></app-nav>
-  <div class="container">
-  <app-front></app-front>
-  <app-stats></app-stats>
-  <app-projects></app-projects>
-  <app-skills></app-skills>
-  <app-e></app-e>
-  <app-footer></app-footer>
-    
-  </div>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz@0,14..32;1,14..32&family=Poppins:wght@600;700&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+    />
+    <link rel="icon" type="image/png" href="./MMA-logo-web.png" />
+    <app-nav></app-nav>
+    <div class="container">
+      <app-front></app-front>
+      <app-stats></app-stats>
+      <app-projects></app-projects>
+      <app-skills></app-skills>
+      <app-e></app-e>
+      <app-footer></app-footer>
+    </div>
   `,
   styleUrl: './app.css',
 })
